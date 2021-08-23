de353c423c1d1309afb5015ad32b104a11c933b2
Vikram Singh <vikram.singh@sarvika.com>
2021-08-23 12:14:43 +0530

    Add apis to swagger

--- src/main/java/com/stackhack/timesheet/config/SwaggerConfig.java
+++ src/main/java/com/stackhack/timesheet/config/SwaggerConfig.java
@@ -18,35 +18,50 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @EnableSwagger2
 public class SwaggerConfig {
 
-  private static final String CONTROLLERS_PACKAGE =
-      "com.stackhack.timesheet.api";
+    private static final String CONTROLLERS_PACKAGE =
+            "com.stackhack.timesheet.api";
 
-  private Docket docket(String pkg, String paths, String groupName) {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .groupName(ObjectUtils.isEmpty(groupName) ? paths : groupName )
-        .select()
-        .apis(RequestHandlerSelectors.basePackage(pkg))
-        .paths(regex(paths))
-        .build()
-        .apiInfo(metadata());
-  }
+    private Docket docket(String pkg, String paths, String groupName) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName(ObjectUtils.isEmpty(groupName) ? paths : groupName)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(pkg))
+                .paths(regex(paths))
+                .build()
+                .apiInfo(metadata());
+    }
 
-  private ApiInfo metadata() {
-    return new ApiInfoBuilder()
-        .title("Time sheet Application Swagger UI")
-        .contact(new Contact("Vikram", "https://sarvika.com", "vikram.singh@sarvika.com"))
-        .build();
-  }
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder()
+                .title("Time sheet Application Swagger UI")
+                .contact(new Contact("Vikram", "https://sarvika.com", "vikram.singh@sarvika.com"))
+                .build();
+    }
 
-  @Bean
-  public Docket projectApiInfo() {
-    return docket(CONTROLLERS_PACKAGE, "/project.*", "Project API");
-  }
+    @Bean
+    public Docket projectApiInfo() {
+        return docket(CONTROLLERS_PACKAGE, "/project.*", "Project API");
+    }
 
 
-  @Bean
-  public Docket costHeadApiInfo() {
-    return docket(CONTROLLERS_PACKAGE, "/costjead.*", "Cost Head API");
-  }
+    @Bean
+    public Docket costHeadApiInfo() {
+        return docket(CONTROLLERS_PACKAGE, "/costjead.*", "Cost Head API");
+    }
+
+    @Bean
+    public Docket userApiInfo() {
+        return docket(CONTROLLERS_PACKAGE, "/user.*", "User API");
+    }
+
+    @Bean
+    public Docket teamApiInfo() {
+        return docket(CONTROLLERS_PACKAGE, "/team.*", "Team API");
+    }
+
+    @Bean
+    public Docket timeApiInfo() {
+        return docket(CONTROLLERS_PACKAGE, "/time.*", "Time Log API");
+    }
 
 }
